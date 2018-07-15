ac59834cd488601918dc90845ee7c622313f9050
Allegra <aukseallegra@gmail.com>
2018-07-15 22:56:41 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file
+// filters a list of names to match driverNames case insensitively:
+
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+// PARTIALLY MATCH SUBSTRINGS:     Sa in Sammy, Sarah, etc.
+function fuzzyMatch (list, lettersToMatch) {
+  let nameLength = lettersToMatch.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, nameLength) === lettersToMatch;
+  });
+}
+
+//  MATCHES OBJECT VALUES TO A PROVIDED STRING:
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
